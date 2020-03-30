5199c5a5113340b1f9705c5581290ff03998da77
CosmoXDD <benjaminsalazarballadares@gmail.com>
2020-03-30 13:45:28 -0300

    Add the source code

---
+++ src/download.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DownloadScreen_cs
+{
+    partial class download
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 54);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(387, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // download
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 149);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "download";
+            this.ShowIcon = false;
+            this.Text = "Download";
+            this.Load += new System.EventHandler(this.download_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ src/download.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
+namespace DownloadScreen_cs
+{
+    public partial class download : Form
+    {
+        public string URL = String.Empty;
+        public string FileName = String.Empty;
+
+        WebClient wc = new WebClient();
+
+        public download()
+        {
+            InitializeComponent();
+        }
+
+        private void download_Load(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                wc.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadComplete);
+                Uri fileurl = new Uri(URL);
+                wc.DownloadFileAsync(fileurl, FileName);
+            });
+
+            thread.Start();
+
+            progressBar1.Minimum = 0;
+        }
+
+        private void DownloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete!", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar1.Minimum = 0;
+                double recieve = double.Parse(e.BytesReceived.ToString());
+                double total = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = recieve / total * 100;
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            }));
+        }
+    }
+}
